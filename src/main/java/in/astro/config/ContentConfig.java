356026826e195140e34b60afd783dfb4b0834dfb
Anubhav Pandey <57632430+SpeedyAstro@users.noreply.github.com>
2024-05-09 14:58:41 +0530

    init

--- src/main/java/in/astro/config/ContentConfig.java
+++ src/main/java/in/astro/config/ContentConfig.java
@@ -3,10 +3,16 @@ package in.astro.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ContentConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
