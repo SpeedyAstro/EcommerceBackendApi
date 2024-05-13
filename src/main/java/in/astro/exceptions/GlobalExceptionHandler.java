bc246de0a689cd1f010a282f7df5d76cf6e84725
Anubhav Pandey <57632430+SpeedyAstro@users.noreply.github.com>
2024-05-13 17:37:36 +0530

    init

--- src/main/java/in/astro/exceptions/GlobalExceptionHandler.java
+++ src/main/java/in/astro/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,15 @@ package in.astro.exceptions;
 import in.astro.payloads.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,6 +30,21 @@ public class GlobalExceptionHandler {
         String message = e.getMessage();
 
         APIResponse res = new APIResponse(message, false);
+
+        return new ResponseEntity<APIResponse>(res, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> myAuthenticationException(AuthenticationException e) {
+
+        String res = e.getMessage();
+
+        return new ResponseEntity<String>(res, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MissingPathVariableException.class)
+    public ResponseEntity<APIResponse> myMissingPathVariableException(MissingPathVariableException e) {
+        APIResponse res = new APIResponse(e.getMessage(), false);
 
         return new ResponseEntity<APIResponse>(res, HttpStatus.BAD_REQUEST);
     }
